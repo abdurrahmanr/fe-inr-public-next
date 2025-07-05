399001c83f0a2be888c2b87977d5976cfa0ff1cb
abdurrahmanr <imanabdurrahmanr@gmail.com>
2025-07-05 21:48:34 +0800

    feat: testing for generated og image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -23,7 +23,7 @@ export default async function Image() {
           justifyContent: "center",
         }}
       >
-        <img src={logoSrc} height="100" />
+        <img src={logoSrc as any} height="100" />
       </div>
     ),
   );
