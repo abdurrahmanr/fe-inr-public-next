53ddfc8cc65d7cfeccb63516e5b02097f1423b41
abdurrahmanr <imanabdurrahmanr@gmail.com>
2025-12-04 15:00:57 +0800

    fix: remove deprecated eslint option from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    trailingSlash: true,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
     images: {
         remotePatterns: [
             {
