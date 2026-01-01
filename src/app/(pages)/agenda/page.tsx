91e627998b913f04dcbf88edd6114a74834dd8d5
ahmadmuslikh <muslikhahmad8@gmail.com>
2026-01-01 17:03:52 +0800

    refactor agenda pages

--- src/app/(pages)/agenda/page.tsx
+++ src/app/(pages)/agenda/page.tsx
@@ -6,10 +6,11 @@ import { fetcher } from "@/utils/fetcher";
 import Image from "next/image";
 import Countdown from "react-countdown";
 import useSWR from "swr";
-import Card from "./_components/card";
+import AgendaSkeleton from "./_components/agenda-skeleton";
+import AgendaCard from "./_components/agenda-card";
 
 const Page = () => {
-    const { data, error, isLoading } = useSWR(`/api/agenda`, fetcher);
+    const { data, isLoading } = useSWR(`/api/agenda`, fetcher);
 
     return (
         <>
@@ -37,7 +38,10 @@ const Page = () => {
                                 <p className="text-[13px]">
                                     Kegiatan Dimulai dalam:
                                 </p>
-                                {/* <Countdown date={data?.data[0].time ?? 0} renderer={Renderer} /> */}
+                                <Countdown
+                                    date={data?.data[0].time ?? 0}
+                                    renderer={Renderer}
+                                />
                             </>
                         )}
                     </div>
@@ -47,7 +51,13 @@ const Page = () => {
                         Agenda yang akan datang
                     </p>
                     <div className="mt-10 grid grid-cols-12 gap-6">
-                        <Card data={data} loading={isLoading} error={error} />
+                        {isLoading ? (
+                            <AgendaSkeleton />
+                        ) : (
+                            data?.data.map((agenda, index: number) => (
+                                <AgendaCard key={index} data={agenda} />
+                            ))
+                        )}
                     </div>
                 </div>
             </div>
